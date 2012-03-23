a0605678ca8ee307c83d00a57fb4aadafb1ac827
Jiho Chang <jiho04.chang@samsung.com>
2012-03-24 06:01:16 +0900

    hardware: exynos5: add initial libswconverter
    Change-Id: I18185959d0a97a56ecfbb13973c52a38e43ac7a0
    Signed-off-by: Jiho Chang <jiho04.chang@samsung.com>
    
    Conflicts:
    	libswconverter/Android.mk
    	libswconverter/swconvertor.c

--- libswconverter/Android.mk
+++ libswconverter/Android.mk
@@ -6,22 +6,16 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
 	swconvertor.c \
-	csc_linear_to_tiled_crop_neon.s \
-	csc_linear_to_tiled_interleave_crop_neon.s \
-	csc_tiled_to_linear_crop_neon.s \
-	csc_tiled_to_linear_deinterleave_crop_neon.s \
+	csc_tiled_to_linear_y_neon.s \
+	csc_tiled_to_linear_uv_neon.s \
+	csc_tiled_to_linear_uv_deinterleave_neon.s \
 	csc_interleave_memcpy_neon.s \
 	csc_ARGB8888_to_YUV420SP_NEON.s
 
 LOCAL_C_INCLUDES := \
 	$(LOCAL_PATH)/../include \
-	$(LOCAL_PATH)/../libhwconverter \
 	$(TOP)/hardware/samsung_slsi/openmax/include/khronos \
-	$(TOP)/hardware/samsung_slsi/openmax/include/sec \
-
-ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
-LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
-endif
+	$(TOP)/hardware/samsung_slsi/openmax/include/$(OMX_NAME) \
 
 LOCAL_MODULE := libswconverter
 
@@ -32,6 +26,6 @@ LOCAL_CFLAGS :=
 LOCAL_ARM_MODE := arm
 
 LOCAL_STATIC_LIBRARIES :=
-LOCAL_SHARED_LIBRARIES := liblog libfimc libhwconverter
+LOCAL_SHARED_LIBRARIES := liblog
 
 include $(BUILD_STATIC_LIBRARY)
