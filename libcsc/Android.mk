ebb42d880dab59ab1cb451520e775d8cbe62f747
Dima Zavin <dima@android.com>
2012-04-16 16:25:38 -0700

    exynos5: libcsc: add simple helper library for hal<->omx format conversions
    Change-Id: Ie687a485ab3538dc8cb60fdc5bf57a5d7be5c297
    Signed-off-by: Dima Zavin <dima@android.com>
    
    Conflicts:
    	libcsc/Android.mk

--- libcsc/Android.mk
+++ libcsc/Android.mk
@@ -1,5 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 
+OMX_NAME := exynos
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    csc_helper.c
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../exynos_omx/openmax/$(OMX_NAME)_omx/include/khronos \
+    $(LOCAL_PATH)/../exynos_omx/openmax/$(OMX_NAME)_omx/include/$(OMX_NAME) \
+    $(LOCAL_PATH)/../include
+
+LOCAL_CFLAGS := \
+    -DUSE_SAMSUNG_COLORFORMAT \
+    -DEXYNOS_OMX
+
+LOCAL_MODULE := libcsc_helper
+LOCAL_MODULE_TAGS := optional
+LOCAL_STATIC_LIBRARIES := liblog
+
+include $(BUILD_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_COPY_HEADERS_TO := libsecmm
@@ -10,8 +32,6 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
 	csc.c
-
-OMX_NAME := exynos
 
 LOCAL_C_INCLUDES := \
 	$(TOP)/hardware/samsung_slsi/openmax/include/khronos \
@@ -26,6 +46,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 
 LOCAL_STATIC_LIBRARIES := libswconverter
+LOCAL_WHOLE_STATIC_LIBRARIES := libcsc_helper
 LOCAL_SHARED_LIBRARIES := liblog libexynosutils
 
 LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
@@ -44,7 +65,6 @@ LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../include
 LOCAL_CFLAGS += -DUSE_GSCALER
 LOCAL_SHARED_LIBRARIES += libexynosgscaler
-endif
 
 LOCAL_CFLAGS += -DUSE_ION
 LOCAL_SHARED_LIBRARIES += libion_exynos
